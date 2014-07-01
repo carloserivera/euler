e327a316781fa4e080b3491fdcabd69e791a2f33
Carlos Rivera <cerivera@fastmail.fm>
2014-06-30 19:29:22 -0500

    adding comments

--- problems/p1.hs
+++ problems/p1.hs
@@ -1,6 +1,7 @@
+-- Sum up all numbers below n that are divisible by 3 or 5
+sumFactors :: Int -> Int
+sumFactors n = sumFactorsReal n 0
+
 sumFactorsReal :: Int -> Int -> Int
 sumFactorsReal 0 sum = sum
 sumFactorsReal n sum = let prevN = (n - 1) in if ((mod prevN 5) == 0) || ((mod prevN 3) == 0) then sumFactorsReal prevN (sum+prevN) else sumFactorsReal prevN sum
-
-sumFactors :: Int -> Int
-sumFactors n = sumFactorsReal n 0
