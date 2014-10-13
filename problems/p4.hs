48cdaf191bda707283c54cce9a3e6550f03b0ca9
Carlos Rivera <cerivera@fastmail.fm>
2014-10-12 21:36:07 -0500

    mminor updates

--- problems/p4.hs
+++ problems/p4.hs
@@ -1,4 +1,8 @@
--- largestPalinedromeProduct 999 999 0
+-- Returns largest palindrome product made by two numbers of size numDigits.
+-- largestPalinedromeProduct numDigits
+-- 
+-- E.g. 
+-- largestPalindromeProduct 3
 -- 906609
 
 isStrPalindrome :: String -> Bool
@@ -6,13 +10,21 @@ isStrPalindrome s = s == (reverse s)
 
 isNumPalindrome n = isStrPalindrome (show n)
 
-largestPalindromeProduct :: Int -> Int -> Int -> Int
-largestPalindromeProduct n1 n2 maxProd = let prod = n1 * n2 in
+largestPalindromeProductAux :: Int -> Int -> Int -> Int
+largestPalindromeProductAux n1 n2 maxProd = let prod = n1 * n2 in
 	if (n2 <= 0) then maxProd
-	else if (n1 <= 0) then largestPalindromeProduct (n2-1) (n2-1) maxProd
-	else if (prod < maxProd) then largestPalindromeProduct (n2-1) (n2-1) maxProd
-	else if (isNumPalindrome prod) then largestPalindromeProduct (n1-1) (n2) prod
-	else largestPalindromeProduct (n1-1) (n2) maxProd
+	else if (n1 <= 0) then largestPalindromeProductAux (n2-1) (n2-1) maxProd
+	else if (prod < maxProd) then largestPalindromeProductAux (n2-1) (n2-1) maxProd
+	else if (isNumPalindrome prod) then largestPalindromeProductAux (n1-1) (n2) prod
+	else largestPalindromeProductAux (n1-1) (n2) maxProd
 
+largestNumGivenNumDigits :: Int -> Int -> Int
+largestNumGivenNumDigits numDigits result = 
+	if (numDigits <= 0) then result
+	else largestNumGivenNumDigits (numDigits - 1) (result + 9*(10 ^ (numDigits - 1)))
 
-
+largestPalindromeProduct :: Int -> Int
+largestPalindromeProduct numDigits = let maxFactor = largestNumGivenNumDigits numDigits 0 in
+	if (maxFactor <= 0) then 0
+	else largestPalindromeProductAux maxFactor maxFactor 0
+	
--- problems/p5.hs
+++ problems/p5.hs
@@ -1,13 +1,19 @@
 -- we should only test positive numbers.
 -- 
 --
+
 smallestMultiple :: Int -> Int
 smallestMultiple maxRange = smallestMultipleAux maxRange maxRange
 
 smallestMultipleAux :: Int -> Int -> Int
-smallestMultipleAux maxRange n = if (isMultipleOfRange maxRange n) then n else smallestMultipleAux maxRange (n+maxRange)
+smallestMultipleAux maxRange n = 
+	if (isMultipleOfRange maxRange n) then n 
+	else smallestMultipleAux maxRange (n+maxRange)
 
 -- Returns true if all numbers from 1 to c are factors of n
 isMultipleOfRange :: Int -> Int -> Bool
-isMultipleOfRange c n = if (c==1) then True else if ((mod n c) /= 0) then False else isMultipleOfRange (c-1) n
+isMultipleOfRange c n = 
+	if (c==1) then True 
+	else if ((mod n c) /= 0) then False 
+	else isMultipleOfRange (c-1) n
 
